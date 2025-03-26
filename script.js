b7990ebc00a755141ac3d84aaecfd4fb64a81808
felipadas07 <felipe.santos.silva22@escola.pr.gov.br>
2025-03-26 12:35:06 -0300

    26 de marco

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const aumentarFonteBotao = document.getElementById('aumentar-fonte') ;
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte') ;
- 
- 
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
-    aumentarFonteBotao. addEventListener('click', function(){
+
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
- 
- 
- 
- 
-    });
-    diminuiFonteBotao.addEventListener('click', function(){
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
- 
- 
-    });
-   
- });
- 
- 
+
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
+})
